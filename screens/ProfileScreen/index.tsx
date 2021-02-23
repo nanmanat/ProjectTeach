ed7d630d4ed485c9358c5644dbbd08b6a4f6c354
nanmanat <53960201+nanmanat@users.noreply.github.com>
2021-02-23 22:31:35 +0700

    Profile page 1st

--- screens/ProfileScreen/index.tsx
+++ screens/ProfileScreen/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import {View, Text, Image} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
 import styles from './style';
 
 const ProfileScreen = () => {
+
+    const [user,setUser] = useState({
+        id: '1',
+        name: "Mill",
+        email: 'nanmanat.v@gmail.com',
+        image: 'https://cdn.discordapp.com/attachments/556849985564573696/813762335289901056/Profile.jpg',
+    })
+
+    const signOut = () => {
+        console.warn("Sign out")
+    } 
+
     return (
         <View style={styles.root}>
-            <Text>Profile Component</Text>
+            <View style={styles.userContianer}>
+                <View style={styles.left}>
+                    <Image style={styles.userImage} source={{ uri: user.image}} />
+                    <View>
+                        <Text style={styles.name}>{user.name}</Text>
+                        <Text style={styles.email}>{user.email}</Text>
+                    </View>
+                </View>
+                <Pressable onPress={signOut} style= {styles.center}>
+                    <Text>Sign Out</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
--- screens/ProfileScreen/style.ts
+++ screens/ProfileScreen/style.ts
@@ -5,14 +5,40 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 20,
         flex: 1,
-        width: '100%',
-        height: 50,
+        backgroundColor: 'white'
     },
-    lable: {
-        fontSize: 18,
+    image: {
+        height: 175,
+        resizeMode: "contain",
+    },
+    userContianer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 50,
+        marginVertical: 10,
+        width: '100%',
+    },
+    userImage: {
+        height: 65,
+        width: 65,
+        marginRight: 10,
+        borderRadius: 50,
+    },
+    name:{
         fontWeight: 'bold',
-        color: '#000000',
-    }
+        marginBottom: 5,
+        fontSize: 18,
+    },
+    email:{
+    },
+    left: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    center: {
+        marginTop: 'auto',
+    } 
 });
 
 export default styles;
